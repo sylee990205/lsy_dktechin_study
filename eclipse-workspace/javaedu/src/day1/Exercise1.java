8b41c0094a43f7afd2bfd3c6ada5722db536b7c8
sylee990205 <sylee.990205@gmail.com>
2023-03-06 17:12:27 +0900

    day1_excercise
    자바 1일차 실습

---
+++ eclipse-workspace/javaedu/src/day1/Exercise1.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 25;
+		int num3 = 33;
+		
+		System.out.println("합계 : " + (num1+num2+num3));
+		System.out.println("평균 : " + (num1+num2+num3)/3);
+
+	}
+
+}
---
+++ eclipse-workspace/javaedu/src/day1/Exercise3.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		char name1 = '이';
+		char name2 = '소';
+		char name3 = '영';
+		
+		System.out.println(name1);
+		System.out.println(name2);
+		System.out.println(name3);
+	}
+
+}
