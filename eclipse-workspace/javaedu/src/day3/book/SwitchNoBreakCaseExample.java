760deb0f5dc65ec7ad5d4f47a633b515e715fce7
sylee990205 <sylee.990205@gmail.com>
2023-03-08 09:32:41 +0900

    ignore add

--- eclipse-workspace/javaedu/src/day3/book/SwitchNoBreakCaseExample.java
+++ eclipse-workspace/javaedu/src/day3/book/SwitchNoBreakCaseExample.java
@@ -2,7 +2,7 @@ package day3.book;
 
 public class SwitchNoBreakCaseExample {
 	public static void main(String[] args) {
-		int time = (int)(Math.random()*4) + 8;  
+		int time = (int)(Math.random()*4) + 8; // 8 ~ 11 
 		System.out.println("[현재시간: " + time + " 시]");
 		
 		switch(time) {
