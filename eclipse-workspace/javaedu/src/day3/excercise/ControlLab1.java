aee5ac0f22fb362ad1ea2d178b433be3d27f478d
sylee990205 <sylee.990205@gmail.com>
2023-03-08 17:27:51 +0900

    add Excercise Q

--- eclipse-workspace/javaedu/src/day3/excercise/ControlLab1.java
+++ eclipse-workspace/javaedu/src/day3/excercise/ControlLab1.java
@@ -4,7 +4,7 @@ public class ControlLab1 {
 
 	public static void main(String[] args) {
 		int ran_num, sum;
-		int repeat_time = 0;
+		int count = 0;
 		
 		while(true) {
 			ran_num = (int)(Math.random()*11)+10;
@@ -14,7 +14,7 @@ public class ControlLab1 {
 					sum += i;
 				}
 				System.out.printf("1부터 %d까지 합 : %d\n",ran_num,sum);
-				repeat_time++;
+				count++;
 			}
 			else if (ran_num == 11 || ran_num == 17 || ran_num == 19) {
 				break;
@@ -23,8 +23,21 @@ public class ControlLab1 {
 				System.out.println("재수행");
 			}
 		}
-		System.out.printf("%d회 반복함",repeat_time);
+		System.out.printf("%d회 반복함",count);
 		
 	}
 
 }
+
+//[ 실습4 ]
+//1. ControlLab1 이라는 클래스를 생성한다.
+//2. 다음 기능을 반복하여 처리하는 자바 프로그램을 구현하는데 제어문 선택은 임의로 한다.
+//
+//   (1) 10부터 20사이의 난수를 하나 추출한다.
+//   (2) 추출된 숫자가 3의 배수이거나 5의 배수이면 
+//  	 1부터 이 숫자까지의 합을
+//              출력하고 다시 난수 추출부터 수행을 시작한다.
+//   (3) 추출된 숫자가 11 이거나 17이거나 19이면 반복문을 종료한 후에
+//	 "x회 반복함" 를 출력하고 종료한다.(여기에서 반복횟수의 기준은 (2)번의 합을 출력한 횟수임)
+//   (4) 위에서 제시한 조건 외의 값이 추출되면 
+//              "재수행" 이라는 메시지를 출력하고 다시 난수 추출부터 수행을 시작한다.
--- eclipse-workspace/javaedu/src/day3/excercise/WhileLab3.java
+++ eclipse-workspace/javaedu/src/day3/excercise/WhileLab3.java
@@ -4,20 +4,20 @@ public class WhileLab3 {
 
 	public static void main(String[] args) {
 		int ran_num;
-		int repeat_time = 0;
+		int count = 0;
 		char alpha;
 		
 		while(true) {
 			ran_num = (int)(Math.random()*31);
 			alpha = 'A';
 			if (ran_num >= 27 || ran_num == 0) {
-				System.out.printf("출력횟수는 %d번 입니다.\n",repeat_time);
+				System.out.printf("출력횟수는 %d번 입니다.\n",count);
 				break;
 			}
 			else {
 				alpha += ran_num - 1;
 				System.out.printf("%d-%c, %d, %#x\n",ran_num,alpha,(int)alpha, (int)alpha);
-				repeat_time++;
+				count++;
 			}
 				
 		}
@@ -25,3 +25,15 @@ public class WhileLab3 {
 	}
 
 }
+
+//[ 실습3 ] - while 문은 무한루프 처리하고 루프를 끝낼 때는 break 문 사용
+//1. WhileLab3 라는 클래스를 생성한다.
+//2. 0부터 30사이의 난수를 추출한다.
+//    추출된 숫자가 1이면 'A', 2 이면 'B', ... 26이면 'Z' 를 출력하는데
+//    계속 난수 추출과 출력을 반복하다가  0이 추출되거나  27~30이 추출되면 반복을 끝낸다.
+//
+//    반복하는 동안 출력형식 :  	1-A, 65, 0x41
+//			5-E, 69, 0x45
+//			   :
+//    마지막에는 "출력횟수는 x 번입니다"를 출력하고 종료한다.
+//    (출력 횟수를 카운팅하는 것은 직접 구현한다.)
