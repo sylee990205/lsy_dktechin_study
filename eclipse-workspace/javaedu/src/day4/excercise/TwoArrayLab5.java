cf9f6c5731b252def953909d488ab5537fafadd2
Soyoung <sylee.990205@gmail.com>
2023-03-09 22:44:37 +0900

    Excercise Java Day4 update

--- eclipse-workspace/javaedu/src/day4/excercise/TwoArrayLab5.java
+++ eclipse-workspace/javaedu/src/day4/excercise/TwoArrayLab5.java
@@ -11,23 +11,23 @@ public class TwoArrayLab5 {
 			}
 			System.out.println();
 		}
-		
-		for(int num = 0; num < 9; num++) {
-			if (num < 5) {
-				twoArr[num][0] = num + 1;
-			}
-			else if (num + 1 >= 6 && num + 1 <= 8) {
-				twoArr[num-4][1] = num + 1;
-			}
-			else
-				twoArr[num-6][2] = num + 1;
-		
-		}
-		// 이거만 
+		// int로 푼 식
+//		for(int num = 0; num < 9; num++) {
+//			if (num < 5) {
+//				twoArr[num][0] = num + 1;
+//			}
+//			else if (num + 1 >= 6 && num + 1 <= 8) {
+//				twoArr[num-4][1] = num + 1;
+//			}
+//			else
+//				twoArr[num-6][2] = num + 1;
+//		
+//		}
+		// 순서도를 사용한 식 
 		for(int j = 0; j < twoArr[0].length; j++) {
 			for (int i = j; i < twoArr.length - j ; i++)
-				twoArr[i][j] = i + (4 * j);
-		}
+				twoArr[i][j] = i + 1 + j * (twoArr.length - j);
+				}
 		
 		System.out.println();
 		for(int i = 0; i < twoArr.length; i++) {
