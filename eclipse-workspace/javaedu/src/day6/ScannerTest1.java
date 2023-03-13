56030ef574f57ef686c546d714470d8df272ba72
sylee990205 <sylee.990205@gmail.com>
2023-03-13 17:52:24 +0900

    Java Day 6

--- eclipse-workspace/javaedu/src/day6/ScannerTest1.java
+++ eclipse-workspace/javaedu/src/day6/ScannerTest1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ScannerTest1 {
 	public static void main(String[] args) {
 		System.out.print("입력 : ");
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //버퍼링 입력, 입력 중 enter를 눌러야 응용프로그램에게 전달
 		String a,b,c,d;
 		a = sc.next();
 		b = sc.next();
--- eclipse-workspace/javaedu/src/day6/ScannerTest4.java
+++ eclipse-workspace/javaedu/src/day6/ScannerTest4.java
@@ -4,10 +4,12 @@ public class ScannerTest4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.printf("데이터를 입력하세요 : ");
-		String str1 = sc.next();
-		String str2 = sc.next();
-		//sc.nextLine();  // 입력버퍼에 남아있는 개행문자를 청소하는 기능
-		String line1 = sc.nextLine();
+		String str1 = sc.next(); // 한 단어만 읽음, 공백 무시
+		String str2 = sc.next(); 
+		//next 라인을 사용하다가 nextLine을 사용하려면 엔터를 쳤을 때 엔터조차 문자로 인식해서 nextLine이 Null 문자열을 반환받음
+		//이 때 한번 nextLine을 사용해주자.
+		sc.nextLine();  // 입력버퍼에 남아있는 개행문자를 청소하는 기능
+		String line1 = sc.nextLine(); //공백까지 다 읽음
 		String line2 = sc.nextLine();
 		System.out.printf("[%s] [%s] [%s] [%s]", str1, str2, line1, line2);
 		sc.close();
