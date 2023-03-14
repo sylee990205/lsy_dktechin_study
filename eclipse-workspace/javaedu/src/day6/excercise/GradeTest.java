fa641ca4a1ef1dc3b2d2b7f47ff3b807e8391836
sylee990205 <sylee.990205@gmail.com>
2023-03-14 09:51:48 +0900

    Java Day6 excercise review
    코드 리뷰 후 수정

--- eclipse-workspace/javaedu/src/day6/excercise/GradeTest.java
+++ eclipse-workspace/javaedu/src/day6/excercise/GradeTest.java
@@ -10,7 +10,8 @@ class GradeExpr{
 	}
 	
 	double getAverage() {
-		return getTotal()/jumsu.length;
+		return getTotal()/ (double) jumsu.length; // getTotal이 정수형이기에 jumsu.length 를 double로 지정해야 소수점 아래 값도 나온다.
+												  // 자바는 int / int 는 무조건 int 몫만 나옴을 기억하자!
 	}
 	
 	int getTotal() {
@@ -56,12 +57,12 @@ public class GradeTest {
 			System.out.print((i+1)+"번째 점수를 입력해주세요: ");
 			score[i] = sc.nextInt();
 		}
-		System.out.print("점수들 : ");
+		System.out.print("\n점수들 : ");
 		MethodLab7.printArray(score);
 		
 		GradeExpr result = new GradeExpr(score);
 		
-		System.out.printf("\n총점: %d\n", result.getTotal());
+		System.out.printf("총점: %d\n", result.getTotal());
 		System.out.printf("평균 : %.2f\n", result.getAverage());
 		System.out.printf("최고 점수 : %d\n",result.getGoodScore());
 		System.out.printf("최저 점수 : %d",result.getBadScore());
