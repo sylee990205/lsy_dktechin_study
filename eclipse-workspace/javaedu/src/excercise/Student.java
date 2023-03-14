f4e03290c27fb347389103c96ecac052dca30343
sylee990205 <sylee.990205@gmail.com>
2023-03-14 17:35:45 +0900

    Java Day7 excercise

---
+++ eclipse-workspace/javaedu/src/excercise/Student.java
@@ -0,0 +1,22 @@
+package excercise;
+
+public class Student extends Human{
+	private String number;
+	private String major;
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, int age, int height, int weight, String number, String major) {
+		super(name, age, height, weight);
+		this.number = number;
+		this.major = major;
+	}
+	
+	@Override
+	public String printInformation() {
+		return super.printInformation() + "\t" + number + "\t" + major;
+	}
+	
+}
