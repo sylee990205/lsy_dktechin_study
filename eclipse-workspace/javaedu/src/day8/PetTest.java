9aa26c2ff843844ac265ea05cb2c7238a8e9451d
sylee990205 <sylee.990205@gmail.com>
2023-03-15 14:47:20 +0900

    move Pettest Day7->Day8

--- eclipse-workspace/javaedu/src/day8/PetTest.java
+++ eclipse-workspace/javaedu/src/day8/PetTest.java
@@ -1,15 +1,15 @@
-package day7;
+package day8;
 
 
 abstract class Pet {
-	public abstract void petSound();
+	public abstract void petSound(); //미완성
 
 	public void sleep() {
 		System.out.println("Zzz");
 	}
 }
 class Dog extends Pet{
-	public void petSound() {
+	public void petSound() { //public 지정 필수, 상속 받을 땐 제어자 범위를 좁힐 수 없다
 		System.out.println("멍멍!!");
 	}
 }
