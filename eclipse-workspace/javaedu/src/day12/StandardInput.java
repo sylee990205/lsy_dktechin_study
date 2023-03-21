eb36b3e33438c9b70fcfca453d7110056b8177d4
sylee990205 <sylee.990205@gmail.com>
2023-03-21 15:15:27 +0900

    Java Day 12

--- eclipse-workspace/javaedu/src/day12/StandardInput.java
+++ eclipse-workspace/javaedu/src/day12/StandardInput.java
@@ -2,9 +2,10 @@ package day12;
 
 public class StandardInput {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		System.out.print("입력 : ");
+		char munja = (char)System.in.read(); // 1문자 1byte만 읽어옴
+		System.out.println("입력된 문자 : " + munja);
 	}
 
 }
